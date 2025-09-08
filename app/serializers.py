d02f3dd0827c68cafd2a0c9fbfc60cfe74b1f6df
Rojas Córsico Ivana Soledad <irojas@gmail.com>
2025-09-08 14:16:57 -0300

    Ejemplo API Rest -DRF

---
+++ app/serializers.py
@@ -0,0 +1,19 @@
+
+from rest_framework import serializers
+from .models import User,Role
+
+class RoleSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Role
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    role = RoleSerializer(read_only=True)
+
+    class Meta:
+        model= User
+        fields = ['id', 'name', 'email', 'dni', 'birth_date', 'role', 'activo']
+
+
